5f908f6865e986b6a7c03e43f2782ceec7a6d370
Jan van der Linden <jan.linden@student.kit.edu>
2020-07-13 20:43:30 +0200

    test calculation

--- configs/test_calculation.py
+++ configs/test_calculation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import common
+from array import array
 
 def get_additional_variables():
     '''
@@ -16,49 +17,59 @@ def get_additional_variables():
         "Evt_ID",
         "Evt_Run",
         "Evt_Lumi"
+        "GenEvt_I_TTPlusBB",
+        "GenEvt_I_TTPlusCC",
+
+        "Jet_DeepCSV_b",
+        "Jet_DeepCSV_bb",
+        "Jet_DeepCSV_c",
+        "Jet_DeepCSV_udsg",
+        "Jet_DeepJet_b",
+        "Jet_DeepJet_bb",
+        "Jet_DeepJet_c",
+        "Jet_DeepJet_g",
+        "Jet_DeepJet_lepb",
+        "Jet_DeepJet_uds",
         ]
-    for i in range(11):
-        variables += [
-            "Jet_DeepCSV_b["+str(i)+"]",
-            "Jet_DeepCSV_bb["+str(i)+"]",
-            "Jet_DeepCSV_c["+str(i)+"]",
-            "Jet_DeepCSV_udsg["+str(i)+"]",
-            "Jet_DeepJet_b["+str(i)+"]",
-            "Jet_DeepJet_bb["+str(i)+"]",
-            "Jet_DeepJet_c["+str(i)+"]",
-            "Jet_DeepJet_g["+str(i)+"]",
-            "Jet_DeepJet_lepb["+str(i)+"]",
-            "Jet_DeepJet_uds["+str(i)+"]",
-            ]
     return variables
 
 def base_selection(event):
     return event.N_Jets>=4
 
-def calculate_variables(df):
+def set_branches(wrapper):
+    wrapper.SetIntVar("Evt_ID")   
+    wrapper.SetIntVar("Evt_Run")   
+    wrapper.SetIntVar("Evt_Lumi")   
+    wrapper.SetIntVar("GenEvt_I_TTPlusBB")   
+    wrapper.SetIntVar("GenEvt_I_TTPlusCC")   
+    wrapper.SetIntVar("N_Jets")   
+
+    wrapper.SetFloatVarArray("Jet_DeepJet_CvsL","N_Jets")
+    wrapper.SetFloatVarArray("Jet_DeepJet_CvsB","N_Jets")
+    wrapper.SetFloatVarArray("Jet_DeepCSV_CvsL","N_Jets")
+    wrapper.SetFloatVarArray("Jet_DeepCSV_CvsB","N_Jets")
+
+def calculate_variables(event, wrapper):
     '''
     calculate additional variables needed
     '''
-    for i in range(11):
-        df["Jet_DeepCSV_CvsL_"+str(i)] = df["Jet_DeepCSV_c["+str(i)+"]"].values / \
-            (df["Jet_DeepCSV_c["+str(i)+"]"].values + \
-             df["Jet_DeepCSV_udsg["+str(i)+"]"].values + 1e-10)
+    wrapper.branchArrays["Evt_ID"][0] = event.Evt_ID
+    wrapper.branchArrays["Evt_Run"][0] = event.Evt_Run
+    wrapper.branchArrays["Evt_Lumi"][0] = event.Evt_Lumi
+    wrapper.branchArrays["GenEvt_I_TTPlusBB"][0] = event.GenEvt_I_TTPlusBB
+    wrapper.branchArrays["GenEvt_I_TTPlusCC"][0] = event.GenEvt_I_TTPlusCC
+    wrapper.branchArrays["N_Jets"][0] = event.N_Jets
 
-        df["Jet_DeepCSV_CvsB_"+str(i)] = df["Jet_DeepCSV_c["+str(i)+"]"].values / \
-            (df["Jet_DeepCSV_c["+str(i)+"]"].values + \
-             df["Jet_DeepCSV_b["+str(i)+"]"].values + \
-             df["Jet_DeepCSV_bb["+str(i)+"]"].values + 1e-10)
+    for jetIdx in range(event.N_Jets):
+        wrapper.branchArrays["Jet_DeepJet_CvsL"][jetIdx] = \
+            event.Jet_DeepJet_c[jetIdx]/(event.Jet_DeepJet_c[jetIdx] + event.Jet_DeepJet_uds[jetIdx] + event.Jet_DeepJet_g[jetIdx])
+        wrapper.branchArrays["Jet_DeepJet_CvsB"][jetIdx] = \
+            event.Jet_DeepJet_c[jetIdx]/(event.Jet_DeepJet_c[jetIdx] + event.Jet_DeepJet_b[jetIdx] + event.Jet_DeepJet_bb[jetIdx] + event.Jet_DeepJet_lepb[jetIdx])
 
+        wrapper.branchArrays["Jet_DeepCSV_CvsL"][jetIdx] = \
+            event.Jet_DeepCSV_c[jetIdx]/(event.Jet_DeepCSV_c[jetIdx] + event.Jet_DeepCSV_udsg[jetIdx])
+        wrapper.branchArrays["Jet_DeepCSV_CvsB"][jetIdx] = \
+            event.Jet_DeepCSV_c[jetIdx]/(event.Jet_DeepCSV_c[jetIdx] + event.Jet_DeepCSV_b[jetIdx] + event.Jet_DeepCSV_bb[jetIdx])
 
-        df["Jet_DeepJet_CvsB_"+str(i)] = df["Jet_DeepJet_c["+str(i)+"]"].values / \
-            (df["Jet_DeepJet_c["+str(i)+"]"].values + \
-             df["Jet_DeepJet_b["+str(i)+"]"].values + \
-             df["Jet_DeepJet_bb["+str(i)+"]"].values + \
-             df["Jet_DeepJet_lepb["+str(i)+"]"].values + 1e-10)
-
-        df["Jet_DeepJet_CvsL_"+str(i)] = df["Jet_DeepJet_c["+str(i)+"]"].values / \
-            (df["Jet_DeepJet_c["+str(i)+"]"].values + \
-             df["Jet_DeepJet_g["+str(i)+"]"].values + \
-             df["Jet_DeepJet_uds["+str(i)+"]"].values + 1e-10)
-    return df
+    return event
 
