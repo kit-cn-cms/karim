39f894e78fd80965f3890f9364f7cb250801f29d
sebwieland <wielandseb@googlemail.com>
2021-09-03 11:39:06 +0200

    update monotop configs

--- configs/calculate_genWeightsUL_monotop.py
+++ configs/calculate_genWeightsUL_monotop.py
@@ -5,15 +5,27 @@ from array import array
 import os
 filepath = os.path.abspath(__file__)
 karimpath = os.path.dirname(os.path.dirname(filepath))
-year = "18"
-sfDir = os.path.join(karimpath, "data", "UL_"+year)
 
 from correctionlib import _core
 
-# initialize pileup SFs
-pileupSFs = weightModules.PileupSFs(os.path.join(sfDir, "pileup.csv"))
-pu_evaluator = _core.CorrectionSet.from_file(os.path.join(sfDir, "pileup.json"))
-puSFs = pu_evaluator["pileup"]
+# initialize old pileup SFs
+# pileupSFs = weightModules.PileupSFs(os.path.join(sfDir, "pileup.csv"))
+
+data = {}
+for year in ["2018", "2017", "2016preVFP", "2016postVFP"]:
+    # short year
+    yearS = year[2:]
+
+    # dict
+    data[year] = {}
+
+    # sf directory
+    sfDir = os.path.join(karimpath, "data", "UL_"+yearS)
+
+    # PU
+    pu_evaluator = _core.CorrectionSet.from_file(os.path.join(sfDir, "pileup.json"))
+    data[year]["PU"] = pu_evaluator["pileup"]
+
 
 def get_additional_variables():
     '''
@@ -64,7 +76,7 @@ def set_branches(wrapper, jec = None):
     
     
 
-def calculate_variables(event, wrapper, sample, jec, genWeights = None):
+def calculate_variables(event, wrapper, sample, jec, dataEra = None, genWeights = None):
     '''
     calculate weights
     '''
@@ -104,14 +116,14 @@ def calculate_variables(event, wrapper, sample, jec, genWeights = None):
         wrapper.branchArrays["fsrDownRel"][0] = getattr(event, "Weight_fsrDown")
     except: pass
 
-    puSF_old = pileupSFs.getSF(getattr(event, "nTruePU"), "central")
-    wrapper.branchArrays["old_pileup"][0] = puSF_old
-    wrapper.branchArrays["old_pileup_up_rel"][0] = pileupSFs.getSF(getattr(event, "nTruePU"), "up")/puSF_old
-    wrapper.branchArrays["old_pileup_down_rel"][0] = pileupSFs.getSF(getattr(event, "nTruePU"), "down")/puSF_old
+    # puSF_old = pileupSFs.getSF(getattr(event, "nTruePU"), "central")
+    # wrapper.branchArrays["old_pileup"][0] = puSF_old
+    # wrapper.branchArrays["old_pileup_up_rel"][0] = pileupSFs.getSF(getattr(event, "nTruePU"), "up")/puSF_old
+    # wrapper.branchArrays["old_pileup_down_rel"][0] = pileupSFs.getSF(getattr(event, "nTruePU"), "down")/puSF_old
 
-    puSF = puSFs.evaluate("central", float(event.nTruePU))
+    puSF = data[dataEra]["PU"].evaluate("central", float(event.nTruePU))
     wrapper.branchArrays["pileup"][0] = puSF
-    wrapper.branchArrays["pileup_up_rel"][0] = puSFs.evaluate("up", float(event.nTruePU))/puSF
-    wrapper.branchArrays["pileup_down_rel"][0] = puSFs.evaluate("down", float(event.nTruePU))/puSF
+    wrapper.branchArrays["pileup_up_rel"][0] = data[dataEra]["PU"].evaluate("up", float(event.nTruePU))/puSF
+    wrapper.branchArrays["pileup_down_rel"][0] = data[dataEra]["PU"].evaluate("down", float(event.nTruePU))/puSF
     return event
 
