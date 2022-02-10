dee908f038ad2e78de9ce5e7de76ac43fec504ee
rufakm <rufa.rafeek@kit.edu>
2022-02-10 13:30:45 +0100

    Z deacy fractions

--- configs/calculate_ttXfractions.py
+++ configs/calculate_ttXfractions.py
@@ -10,11 +10,13 @@ def get_additional_variables():
     which are needed for the dnn inputs                                                                                                                                 
     '''
     variables = [
+        "event",
         "genJet_jetMatcherClass",
         "genJet_hadronFlavour",
         "nGenJets",
         "nGenBJets",
-        "nGenCJets"
+        "nGenCJets",
+        #"Weight_GEN"
         ]
     return variables
 
@@ -22,26 +24,46 @@ def base_selection(event):
     return True
 
 def set_branches(wrapper):
+    wrapper.SetIntVar("event")
     wrapper.SetIntVar("genJet_jetMatcherClass")
     wrapper.SetIntVar("genJet_hadronFlavour")
     wrapper.SetIntVar("nGenJets")
     wrapper.SetIntVar("nGenBJets")
     wrapper.SetIntVar("nGenCJets")
 
-    wrapper.SetIntVar("isZbb")
-    wrapper.SetIntVar("isZcc")
+    wrapper.SetFloatVar("isZbb")
+    wrapper.SetFloatVar("isZcc")
+    wrapper.SetFloatVar("nZbb")
+    wrapper.SetFloatVar("nZcc")
+   # wrapper.SetFloatVar("Zbb_frac")
+   # wrapper.SetFloatVar("Zcc_frac")
+    
    # wrapper.SetIntVar("isZbb","nGenBJets")
    # wrapper.SetIntVar("isZcc","nGenCJets")
-
+   #wrapper.SetFloatVar("Weight_GEN")
 
 def calculate_variables():
     zJetFlavours = []
+    isZbb = 1.
+    isZcc = 1.
+    nZbb  = 0
+    nZcc  = 0
     for i in nGenJets:
         if genJet_jetMatcherClass[i] == 5: zjetFlavours.append(genjet_hadronFlavour[i])
 
     if all(zJetFlavours==5):   isZbb = 1
     elif all(zJetFlavours==4): isZcc = 1
 
+    for i in zJetFlavours:
+        if zJetFlavours[i]==5:
+            #isZbb[i] = 1
+            nZbb    += 1
+        if zJetFlavours[i]==4:
+            #isZcc[i] = 1
+            nZcc    += 1
+    
+    isZbb = nZbb/len(event.Weight_GEN)
+    isZcc = nZcc/len(event.Weight_GEN)
     
     return event
 
