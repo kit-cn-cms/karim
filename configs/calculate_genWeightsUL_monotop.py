79410a491c5780e27ae2d07cb8dac7494f8992e8
sebwieland <wielandseb@googlemail.com>
2022-05-09 12:44:01 +0200

    monotop: add kFactors to genWeights config

--- configs/calculate_genWeightsUL_monotop.py
+++ configs/calculate_genWeightsUL_monotop.py
@@ -1,6 +1,7 @@
 
 
 
+import ROOT
 import numpy as np
 import common
 import weightModules
@@ -8,6 +9,33 @@ from array import array
 import os
 filepath = os.path.abspath(__file__)
 karimpath = os.path.dirname(os.path.dirname(filepath))
+
+ROOT.TH1.AddDirectory(False)
+kFactors_QCD = {}
+kFactors_EW = {}
+kFactors_EW1 = {}
+kFactors_EW2 = {}
+kFactors_EW3 = {}
+for mode in ["evj","eej", "vvj", "aj"]:
+    filePath = os.path.join(karimpath, "data", "kFactors", mode + ".root")
+    file = ROOT.TFile(filePath)
+    nnlo = file.Get(mode + "_pTV_K_NNLO").Clone()
+    nlo = file.Get(mode + "_pTV_K_NLO").Clone()
+    ew = file.Get(mode + "_pTV_kappa_EW").Clone()
+    ew1 = file.Get(mode + "_pTV_d1kappa_EW").Clone()
+    ew2 = file.Get(mode + "_pTV_d2kappa_EW").Clone()
+    ew3 = file.Get(mode + "_pTV_d3kappa_EW").Clone()
+    nnlo.Divide(nlo)
+    kFactors_QCD[mode] = nnlo.Clone(mode)
+    kFactors_EW[mode] = ew.Clone()
+    kFactors_EW1[mode] = ew1.Clone()
+    kFactors_EW2[mode] = ew2.Clone()
+    kFactors_EW3[mode] = ew3.Clone()
+    print("QCD:", kFactors_QCD)
+    print("EW", kFactors_EW)
+    print("EW1", kFactors_EW1)
+    print("EW2", kFactors_EW2)
+    print("EW3", kFactors_EW3)
 
 from correctionlib import _core
 jsonDir = os.path.join("/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration", "POG")
@@ -75,6 +103,17 @@ def set_branches(wrapper, jec = None):
     wrapper.SetFloatVar("pdf_down")
     wrapper.SetFloatVar("pdf_down_rel")
 
+    wrapper.SetFloatVar("Boson_Pt") 
+    for mode in ["evj","eej", "vvj", "aj"]:
+        wrapper.SetFloatVar( mode + "_kFactor_QCD")
+        wrapper.SetFloatVar( mode + "_kFactor_EW")
+        wrapper.SetFloatVar( mode + "_kFactor_EW1up")
+        wrapper.SetFloatVar( mode + "_kFactor_EW1down")
+        wrapper.SetFloatVar( mode + "_kFactor_EW2up")
+        wrapper.SetFloatVar( mode + "_kFactor_EW2down")
+        wrapper.SetFloatVar( mode + "_kFactor")
+
+
 def calculate_variables(event, wrapper, sample, jec, dataEra = None, genWeights = None):
     '''
     calculate weights
@@ -119,6 +158,68 @@ def calculate_variables(event, wrapper, sample, jec, dataEra = None, genWeights 
     wrapper.branchArrays["pileup"][0] = pu
     wrapper.branchArrays["pileup_up_rel"][0] = puSF[dataEra].evaluate(float(event.nTruePU), "up")/pu
     wrapper.branchArrays["pileup_down_rel"][0] = puSF[dataEra].evaluate(float(event.nTruePU), "down")/pu
+
+    # calculate kFactors
+    result = {}
+    for mode in ["evj","eej", "vvj", "aj"]:
+        result[mode] = {
+            "kFactor_QCD": 1.,
+            "kFactor_EW": 1.,
+            "kFactor_EW1_up": 1.,
+            "kFactor_EW1_down": 1.,
+            "kFactor_EW2_up": 1.,
+            "kFactor_EW2_down": 1.,
+            "kFactor_EW3_up": 1.,
+            "kFactor_EW3_down": 1.,
+        }
+
+
+    pT = -1.
+    if "ToLNu" in sample:
+        label = "evj"
+        if getattr(event, "N_wBosons") >= 1:
+            pT = getattr(event, "wBoson_Pt")[0]
+    elif "ToLL" in sample:
+        label = "eej"
+        if getattr(event, "N_zBosons") >= 1:
+            pT = getattr(event, "zBoson_Pt")[0]
+    elif "NuNu" in sample:
+        label = "vvj"
+        if getattr(event, "N_zBosons") >= 1:
+            pT = getattr(event, "zBoson_Pt")[0]
+
+    if  pT >= 30.:
+        b = kFactors_QCD[label].FindBin(pT)
+        result[label]["kFactor_QCD"] = kFactors_QCD[label].GetBinContent(b)
+
+        b = kFactors_EW[label].FindBin(pT)
+        nom = 1. + kFactors_EW[label].GetBinContent(b)
+        ew1 = kFactors_EW1[label].GetBinContent(b)
+        ew2 = kFactors_EW2[label].GetBinContent(b)
+        ew3 = kFactors_EW3[label].GetBinContent(b)
+
+        result[label]["kFactor_EW"] = nom
+        
+        result[label]["kFactor_EW1_up"] = (nom + ew1)
+        result[label]["kFactor_EW1_down"] = (nom - ew1) / nom
+
+        result[label]["kFactor_EW2_up"] = (nom + ew2)
+        result[label]["kFactor_EW2_down"] = (nom - ew2)
+
+        result[label]["kFactor_EW3_up"] = (nom + ew3)
+        result[label]["kFactor_EW3_down"] = (nom - ew3)
+
+    wrapper.branchArrays["Boson_Pt"][0] = pT
+    
+
+    for mode in ["evj","eej", "vvj", "aj"]:
+        wrapper.branchArrays[ mode + "_kFactor_QCD"][0] = result[mode]["kFactor_QCD"]
+        wrapper.branchArrays[ mode + "_kFactor_EW"][0] = result[mode]["kFactor_EW"]
+        wrapper.branchArrays[ mode + "_kFactor_EW1up"][0] = result[mode]["kFactor_EW1_up"]
+        wrapper.branchArrays[ mode + "_kFactor_EW1down"][0] = result[mode]["kFactor_EW1_down"]
+        wrapper.branchArrays[ mode + "_kFactor_EW2up"][0] = result[mode]["kFactor_EW2_up"]
+        wrapper.branchArrays[ mode + "_kFactor_EW2down"][0] = result[mode]["kFactor_EW2_down"]
+        wrapper.branchArrays[ mode + "_kFactor"][0] =  result[mode]["kFactor_QCD"]*result[mode]["kFactor_EW"] 
 
 
     # simple pdf weight
