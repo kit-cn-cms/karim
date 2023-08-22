8089ad8fc39cbccbe68e8e92373b45ece343ffe3
Michael Wassmer <michael.wassmer@cern.ch>
2023-08-22 17:31:34 +0200

    also add again hadronic btag weights and recoil trigger sfs

--- configs/calculate_FriendsJEC_monotop.py
+++ configs/calculate_FriendsJEC_monotop.py
@@ -52,6 +52,9 @@ pprint(btagEff)
 SFb_sys = ["up_correlated","up_uncorrelated","down_correlated","down_uncorrelated"]
 SFl_sys = ["up_correlated","up_uncorrelated","down_correlated","down_uncorrelated"]
 
+# systematic variations of the recoil trigger scale factors
+sys_recoil_triggersf = ["up","down","statup","statdown","systup","systdown"]
+
 # function to define which branches from the ntuples are needed
 # accepts * as wildcard
 def load_input_branches():
@@ -354,165 +357,178 @@ def calculate_variables(events, wrapper, sample, jecs, dataEra = None, genWeight
     # print("\n\n")
     # print(output_array)
 
-    return output_array
+    
     ################
     ### HADRONIC ###
     ################
-    P_MC   = 1.
+
+    # similar to leptonic, only difference is other jet variable names and light (L) working instead of medium (M) wp
+
+    P_MC   = None
     P_DATA_sys = {}
     sf_L_sys = {}
-    if jec == "nom":
-        for sys in SFb_sys+SFl_sys:
-            P_DATA_sys[sys] = 1.
+    for sys in SFb_sys+SFl_sys+["central"]:
+        P_DATA_sys[sys] = None
+        sf_L_sys[sys] = None
 
-    # necessary jet variables
-    eta   = np.absolute(event["Jets_outside_lead_AK15Jet_Eta"+suffix][0])
-    pt    = event["Jets_outside_lead_AK15Jet_Pt"+suffix][0]
-    flav  = event["Jets_outside_lead_AK15Jet_HadronFlav"+suffix][0]
-    passes_L = event["Jets_outside_lead_AK15Jet_taggedL"+suffix][0]
-
-    # create some masks
-    mask_flav_0 = flav == 0
-    #print("flav 0 mask:",mask_flav_0)
-    mask_flav_4_5 = np.logical_or(flav == 4, flav == 5)
-
-    # use correctionlib
-    eff_L = np.concatenate((btagEff[dataEra]["had"].evaluate("L", flav[mask_flav_0], eta[mask_flav_0], pt[mask_flav_0]),\
-                            btagEff[dataEra]["had"].evaluate("L", flav[mask_flav_4_5], eta[mask_flav_4_5], pt[mask_flav_4_5])))
-    # print("eff M",eff_M)
-    # todo: check empty bins in efficiency
-    eff_L[eff_L == 0.] = 0.001
-
-    # use correctionlib
-    sf_L_sys["central"] = np.concatenate((btagSF[dataEra_lightSF]["deepJet_incl"].evaluate("central", "L", flav[mask_flav_0], eta[mask_flav_0], pt[mask_flav_0]),\
-                                          btagSF[dataEra]["deepJet_comb"].evaluate("central", "L", flav[mask_flav_4_5], eta[mask_flav_4_5], pt[mask_flav_4_5])))
-    if jec == "nom":
-        for sys in SFl_sys:
-            sf_L_sys[sys] = np.concatenate((btagSF[dataEra_lightSF]["deepJet_incl"].evaluate(sys, "L", flav[mask_flav_0], eta[mask_flav_0], pt[mask_flav_0]),\
-                                     btagSF[dataEra]["deepJet_comb"].evaluate("central", "L", flav[mask_flav_4_5], eta[mask_flav_4_5], pt[mask_flav_4_5])))
-        for sys in SFb_sys:
-            sf_L_sys[sys] = np.concatenate((btagSF[dataEra_lightSF]["deepJet_incl"].evaluate("central", "L", flav[mask_flav_0], eta[mask_flav_0], pt[mask_flav_0]),\
-                                     btagSF[dataEra]["deepJet_comb"].evaluate(sys, "L", flav[mask_flav_4_5], eta[mask_flav_4_5], pt[mask_flav_4_5])))
     
-    # rearrange arrays
-    eta = np.concatenate((eta[mask_flav_0],eta[mask_flav_4_5]))
-    pt = np.concatenate((pt[mask_flav_0],pt[mask_flav_4_5]))
-    passes_L = np.concatenate((passes_L[mask_flav_0],passes_L[mask_flav_4_5]))
-    flav = np.concatenate((flav[mask_flav_0],flav[mask_flav_4_5]))
+    eta = []
+    pt = []
+    flav = []
+    passes_L = []
+
+    for event in events:
+        eta_ = []
+        pt_ = []
+        flav_ = []
+        passes_L_ = []
+        for i,jec in enumerate(jecs):
+            suffix = "_"+jec
+            eta_.append(np.absolute(event["Jets_outside_lead_AK15Jet_Eta"+suffix]))
+            pt_.append(event["Jets_outside_lead_AK15Jet_Pt"+suffix])
+            flav_.append(event["Jets_outside_lead_AK15Jet_HadronFlav"+suffix])
+            passes_L_.append(event["Jets_outside_lead_AK15Jet_taggedL"+suffix])
+        eta.append(ak.Array(eta_))
+        pt.append(ak.Array(pt_))
+        flav.append(ak.Array(flav_))
+        passes_L.append(ak.Array(passes_L_))
+    
+    eta = ak.Array(eta)
+    pt = ak.Array(pt)
+    flav = ak.Array(flav)
+    passes_L = ak.Array(passes_L)
+
+    mask_flav_0 = flav == 0
+    mask_flav_4_5 = np.logical_or(flav == 4, flav == 5)
+    
+    eta_lfjets = eta[mask_flav_0]
+    pt_lfjets = pt[mask_flav_0]
+    flav_lfjets = flav[mask_flav_0]
+    eta_hfjets = eta[mask_flav_4_5]
+    pt_hfjets = pt[mask_flav_4_5]
+    flav_hfjets = flav[mask_flav_4_5]
+
+    nums_njets_lf = ak.flatten(ak.num(eta_lfjets,axis=2))
+    nums_njecs_lf = ak.num(eta_lfjets,axis=1)
+    nums_njets_hf = ak.flatten(ak.num(eta_hfjets,axis=2))
+    nums_njecs_hf = ak.num(eta_hfjets,axis=1)
+
+    eta_lfjets_flattened = ak.flatten(ak.flatten(eta_lfjets,axis=2),axis=1)
+    pt_lfjets_flattened = ak.flatten(ak.flatten(pt_lfjets,axis=2),axis=1)
+    flav_lfjets_flattened = ak.flatten(ak.flatten(flav_lfjets,axis=2),axis=1)
+    eta_hfjets_flattened = ak.flatten(ak.flatten(eta_hfjets,axis=2),axis=1)
+    pt_hfjets_flattened = ak.flatten(ak.flatten(pt_hfjets,axis=2),axis=1)
+    flav_hfjets_flattened = ak.flatten(ak.flatten(flav_hfjets,axis=2),axis=1)
+
+    eff_L_lfjets = btagEff[dataEra]["had"].evaluate("L", flav_lfjets_flattened, eta_lfjets_flattened, pt_lfjets_flattened)
+    eff_L_hfjets = btagEff[dataEra]["had"].evaluate("L", flav_hfjets_flattened, eta_hfjets_flattened, pt_hfjets_flattened)
+
+    eff_L_lfjets = ak.unflatten(eff_L_lfjets,nums_njets_lf)
+    eff_L_lfjets = ak.unflatten(eff_L_lfjets,nums_njecs_lf)
+    eff_L_hfjets = ak.unflatten(eff_L_hfjets,nums_njets_hf)
+    eff_L_hfjets = ak.unflatten(eff_L_hfjets,nums_njecs_hf)
+
+    eff_L = ak.concatenate((eff_L_lfjets,eff_L_hfjets),axis=2)
+
+    sf_L_sys_lfjets = btagSF[dataEra_lightSF]["deepJet_incl"].evaluate("central", "L", flav_lfjets_flattened, eta_lfjets_flattened, pt_lfjets_flattened)
+    sf_L_sys_hfjets = btagSF[dataEra]["deepJet_comb"].evaluate("central", "L", flav_hfjets_flattened, eta_hfjets_flattened, pt_hfjets_flattened)
+    sf_L_sys_lfjets = ak.unflatten(ak.unflatten(sf_L_sys_lfjets,nums_njets_lf),nums_njecs_lf)
+    sf_L_sys_hfjets = ak.unflatten(ak.unflatten(sf_L_sys_hfjets,nums_njets_hf),nums_njecs_hf)
+
+    sf_L_sys["central"] = ak.concatenate((sf_L_sys_lfjets,sf_L_sys_hfjets),axis=2)
+
+    for sys in SFl_sys:
+        sf_L_sys_lfjets = btagSF[dataEra_lightSF]["deepJet_incl"].evaluate(sys, "L", flav_lfjets_flattened, eta_lfjets_flattened, pt_lfjets_flattened)
+        sf_L_sys_hfjets = btagSF[dataEra]["deepJet_comb"].evaluate("central", "L", flav_hfjets_flattened, eta_hfjets_flattened, pt_hfjets_flattened)
+        sf_L_sys_lfjets = (ak.unflatten(ak.unflatten(sf_L_sys_lfjets,nums_njets_lf),nums_njecs_lf))[:,index_jec_nom]
+        sf_L_sys_hfjets = (ak.unflatten(ak.unflatten(sf_L_sys_hfjets,nums_njets_hf),nums_njecs_hf))[:,index_jec_nom]
+        sf_L_sys[sys] = ak.concatenate((sf_L_sys_lfjets,sf_L_sys_hfjets),axis=1)
+    
+    for sys in SFb_sys:
+        sf_L_sys_lfjets = btagSF[dataEra_lightSF]["deepJet_incl"].evaluate("central", "L", flav_lfjets_flattened, eta_lfjets_flattened, pt_lfjets_flattened)
+        sf_L_sys_hfjets = btagSF[dataEra]["deepJet_comb"].evaluate(sys, "L", flav_hfjets_flattened, eta_hfjets_flattened, pt_hfjets_flattened)
+        sf_L_sys_lfjets = (ak.unflatten(ak.unflatten(sf_L_sys_lfjets,nums_njets_lf),nums_njecs_lf))[:,index_jec_nom]
+        sf_L_sys_hfjets = (ak.unflatten(ak.unflatten(sf_L_sys_hfjets,nums_njets_hf),nums_njecs_hf))[:,index_jec_nom]
+        sf_L_sys[sys] = ak.concatenate((sf_L_sys_lfjets,sf_L_sys_hfjets),axis=1)
+
+    eta = ak.concatenate((eta_lfjets,eta_hfjets),axis=2)
+    pt = ak.concatenate((pt_lfjets,pt_hfjets),axis=2)
+    passes_L = ak.concatenate((passes_L[mask_flav_0],passes_L[mask_flav_4_5]),axis=2)
+    flav = ak.concatenate((flav_lfjets,flav_hfjets),axis=2)
 
     passes_L_mask = passes_L==1
     fails_L_mask = passes_L==0
-    P_MC = np.prod(eff_L[passes_L_mask])*np.prod(np.add(np.negative(eff_L[fails_L_mask]),1.))
-    P_DATA_sys["central"] = np.prod(np.multiply(eff_L[passes_L_mask],sf_L_sys["central"][passes_L_mask]))*np.prod(np.add(np.negative(np.multiply(eff_L[fails_L_mask],sf_L_sys["central"][fails_L_mask])),1.))
 
-    if jec == "nom":
-        for sys in SFl_sys+SFb_sys:
-            P_DATA_sys[sys] = np.prod(np.multiply(eff_L[passes_L_mask],sf_L_sys[sys][passes_L_mask]))* \
-                              np.prod(np.add(np.negative(np.multiply(eff_L[fails_L_mask],sf_L_sys[sys][fails_L_mask])),1.))
+    eff_L_passes_L = eff_L[passes_L_mask]
+    eff_L_fails_L = eff_L[fails_L_mask]
 
-    wrapper.branchArrays["fixedWPSF_hadronic"+suffix][0] = P_DATA_sys["central"]/P_MC
-    # print("sf central:",P_DATA_sys["central"]/P_MC)
-    if jec == "nom":
-        for sys in SFl_sys:
-            wrapper.branchArrays["fixedWPSFl_hadronic_"+sys+"_rel"][0] = P_DATA_sys[sys]/P_DATA_sys["central"]
-            # print("sf {}".format(sys),P_DATA_sys[sys]/P_DATA_sys["central"])
-        for sys in SFb_sys:
-            wrapper.branchArrays["fixedWPSFb_hadronic_"+sys+"_rel"][0] = P_DATA_sys[sys]/P_DATA_sys["central"]
-            # print("sf {}".format(sys),P_DATA_sys[sys]/P_DATA_sys["central"])
-
-    # for idx in range(event["N_Jets_outside_lead_AK15Jet"+suffix][0]):
-    #     eta   = float(abs(event["Jets_outside_lead_AK15Jet_Eta"+suffix][0][idx]))
-    #     pt    = float(event["Jets_outside_lead_AK15Jet_Pt"+suffix][0][idx])
-    #     flav  = int(event["Jets_outside_lead_AK15Jet_HadronFlav"+suffix][0][idx])
-    #     passes_L = event["Jets_outside_lead_AK15Jet_taggedL"+suffix][0][idx]
-
-    #     # TODO: fix this
-    #     if abs(flav) > 5:
-    #         continue
-    #         # flav = 0
-    #     if abs(eta) > 5:
-    #         continue
-    #         # eta = 0.
-
-    #     eff_L = btagEff[dataEra]["had"].evaluate("L", flav, eta, pt)
-
-
-    #     if flav == 0:
-    #         sf_L = btagSF[dataEra_lightSF]["deepJet_incl"].evaluate("central", "L", flav, eta, pt)
-    #         if jec == "nom":
-    #             for sys in SFl_sys:
-    #                 sfl_L[sys] = btagSF[dataEra_lightSF]["deepJet_incl"].evaluate(sys, "L", flav, eta, pt)
-    #     else:
-    #         sf_L = btagSF[dataEra]["deepJet_comb"].evaluate("central", "L", flav, eta, pt)
-    #         if jec == "nom":
-    #             for sys in SFb_sys:
-    #                 sfb_L[sys] = btagSF[dataEra]["deepJet_comb"].evaluate(sys, "L", flav, eta, pt)
-
-    #     if passes_L:
-    #         P_MC   *= eff_L
-    #         P_DATA *= eff_L*sf_L
-    #         if jec == "nom":
-    #             if flav == 0:
-    #                 for sys in SFl_sys:
-    #                     Pl_DATA[sys] *= eff_L*sfl_L[sys]
-    #                 for sys in SFb_sys:
-    #                     Pb_DATA[sys] *= eff_L*sf_L
-    #             else:
-    #                 for sys in SFb_sys:
-    #                     Pb_DATA[sys] *= eff_L*sfb_L[sys]
-    #                 for sys in SFl_sys:
-    #                     Pl_DATA[sys] *= eff_L*sf_L
-    #     else:
-    #         P_MC   *= (1. - eff_L)
-    #         P_DATA *= (1. - eff_L*sf_L)  
-    #         if jec == "nom":
-    #             if flav == 0:
-    #                 for sys in SFl_sys:
-    #                     Pl_DATA[sys] *= (1. - eff_L*sfl_L[sys])
-    #                 for sys in SFb_sys:
-    #                     Pb_DATA[sys] *= (1. - eff_L*sf_L)
-    #             else:
-    #                 for sys in SFb_sys:
-    #                     Pb_DATA[sys] *= (1. - eff_L*sfb_L[sys])
-    #                 for sys in SFl_sys:
-    #                     Pl_DATA[sys] *= (1. - eff_L*sf_L)
-
-    # wrapper.branchArrays["fixedWPSF_hadronic"+suffix][0] = P_DATA/P_MC
-    # if jec == "nom":
-    #     for sys in SFl_sys:
-    #         wrapper.branchArrays["fixedWPSFl_hadronic_"+sys+"_rel"][0] = Pl_DATA[sys]/P_DATA
-    #     for sys in SFb_sys:
-    #         wrapper.branchArrays["fixedWPSFb_hadronic_"+sys+"_rel"][0] = Pb_DATA[sys]/P_DATA
-
+    P_MC = ak.prod(eff_L_passes_L,axis=2)*ak.prod(np.add(np.negative(eff_L_fails_L),1.),axis=2)
+    P_DATA_sys["central"] = ak.prod(eff_L_passes_L*sf_L_sys["central"][passes_L_mask],axis=2)*\
+                            ak.prod(np.add(np.negative(eff_L_fails_L*sf_L_sys["central"][fails_L_mask]),1.),axis=2)
+    
+    for sys in SFl_sys+SFb_sys:
+        P_DATA_sys[sys] = ak.prod(eff_L_passes_L[:,index_jec_nom]*sf_L_sys[sys][passes_L[:,index_jec_nom]==1],axis=1)* \
+                          ak.prod(np.add(np.negative((eff_L_fails_L[:,index_jec_nom])*(sf_L_sys[sys][passes_L[:,index_jec_nom]==0])),1.),axis=1)
+    
+    for i,jec in enumerate(jecs):
+        suffix = "_"+jec
+        output_array["fixedWPSF_hadronic"+suffix] = P_DATA_sys["central"][:,i]/P_MC[:,i]
+    
+    for sys in SFl_sys:
+        output_array["fixedWPSFl_hadronic_"+sys+"_rel"] = P_DATA_sys[sys]/P_DATA_sys["central"][:,index_jec_nom]
+    for sys in SFb_sys:
+        output_array["fixedWPSFb_hadronic_"+sys+"_rel"] = P_DATA_sys[sys]/P_DATA_sys["central"][:,index_jec_nom]
+    
 
     ##########################
     ### Recoil Trigger SFs ###
     ##########################
+    
+    recoilTriggerSF = {}
+    recoil = None
+    # put recoils into structure events->jec variations
+    for i,jec in enumerate(jecs):
+        suffix = "_"+jec
+        output_array["recoil"+suffix] = np.reshape(events["Hadr_Recoil_MET_T1XY_Pt"+suffix],(nevents,1))
+        if i==0:
+            recoil = output_array["recoil"+suffix]
+        else:
+            recoil = np.concatenate((recoil,output_array["recoil"+suffix]),axis=1)
+    
+    # evaluate recoil trigger sfs using correctionlib
+    recoilTriggerSF["central"] = btagEff[dataEra]["recoilTrig"].evaluate("central", recoil)
 
-    recoil = event["Hadr_Recoil_MET_T1XY_Pt"+suffix][0]
-    wrapper.branchArrays["recoil"+suffix][0] = recoil
+    # consider systematics
+    for sys in sys_recoil_triggersf:
+        recoilTriggerSF[sys] = btagEff[dataEra]["recoilTrig"].evaluate(sys, recoil[:,index_jec_nom])
+    
+    # number of loose leptons and photons for selection mask
+    nmuons = events["N_LooseMuons"]
+    nelectrons = events["N_LooseElectrons"]
+    nphotons = events["N_LoosePhotons"]
 
-    if (recoil > 250) and (event["N_LooseMuons"][0] >= 0 and event["N_LooseElectrons"][0] == 0 and event["N_LoosePhotons"][0] == 0):
-        wrapper.branchArrays["recoilTriggerSF"+suffix][0] = btagEff[dataEra]["recoilTrig"].evaluate("central", recoil)
-        # wrapper.branchArrays["recoilTriggerSF"+suffix+"_up"][0] = btagEff[dataEra]["recoilTrig"].evaluate("up", recoil)
-        # wrapper.branchArrays["recoilTriggerSF"+suffix+"_down"][0] = btagEff[dataEra]["recoilTrig"].evaluate("down", recoil)
+    # selection masks
+    mask_nlep_npho = np.logical_not((nmuons>=0) & (nelectrons==0) & (nphotons==0))
+    mask_recoil = np.logical_not(recoil>=250.)
+    mask_recoil_nom = np.logical_not(recoil[:,index_jec_nom]>=250.)
 
-        # wrapper.branchArrays["recoilTriggerSF"+suffix+"_Statup"][0] = btagEff[dataEra]["recoilTrig"].evaluate("statup", recoil)
-        # wrapper.branchArrays["recoilTriggerSF"+suffix+"_Statdown"][0] = btagEff[dataEra]["recoilTrig"].evaluate("statdown", recoil)
-
-        # wrapper.branchArrays["recoilTriggerSF"+suffix+"_Systup"][0] = btagEff[dataEra]["recoilTrig"].evaluate("systup", recoil)
-        # wrapper.branchArrays["recoilTriggerSF"+suffix+"_Systdown"][0] = btagEff[dataEra]["recoilTrig"].evaluate("systdown", recoil)
-    else:
-        wrapper.branchArrays["recoilTriggerSF"+suffix][0] = 1.
-        # wrapper.branchArrays["recoilTriggerSF"+suffix+"_up"][0] = 1.
-        # wrapper.branchArrays["recoilTriggerSF"+suffix+"_down"][0] = 1.
-
-        # wrapper.branchArrays["recoilTriggerSF"+suffix+"_Statup"][0] = 1.
-        # wrapper.branchArrays["recoilTriggerSF"+suffix+"_Statdown"][0] = 1.
-
-        # wrapper.branchArrays["recoilTriggerSF"+suffix+"_Systup"][0] = 1.
-        # wrapper.branchArrays["recoilTriggerSF"+suffix+"_Systdown"][0] = 1.
+    # use masks to set sf to 1 if event does not fulfill selections
+    recoilTriggerSF["central"][mask_nlep_npho,:] = 1.
+    recoilTriggerSF["central"][mask_recoil] = 1.
+    for sys in sys_recoil_triggersf:
+        recoilTriggerSF[sys][mask_nlep_npho] = 1.
+        recoilTriggerSF[sys][mask_recoil_nom] = 1.
 
 
-    return event
+    # write recoil trigger sfs to output dict
+    for i,jec in enumerate(jecs):
+        suffix = "_"+jec
+        output_array["recoilTriggerSF"+suffix] = recoilTriggerSF["central"][:,i]
+
+    # consider systematics
+    for sys in sys_recoil_triggersf:
+        output_array["recoilTriggerSF_nom_"+sys] = recoilTriggerSF[sys]
+
+    # return output dict
+    return output_array
 
